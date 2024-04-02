d4942cf522c177a3d3ed6d24355dae710b72537e
lorenzosantini05 <105849734+lorenzosantini05@users.noreply.github.com>
2024-04-02 23:48:53 +0200

    Route completata + grafica marker selezionato e dettagli

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,33 +4,160 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 import { DataStorageService } from '../shared/data-storage.service';
 import { InfoWindowComponent } from './info-window/info-window.component';
 import { MapService } from '../shared/map.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, GoogleMapsModule, InfoWindowComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, GoogleMapsModule, InfoWindowComponent, NgClass],
 })
 export class HomePage {
   constructor(private dataStorage: DataStorageService, private mapService:MapService) {}
-
+  flag:boolean = false;
+  vallauri:string = "../assets/icon/vallauri.png"
   markerList:any[] = [];
   //map options
   center: google.maps.LatLngLiteral = { lat: 44.55577411467918, lng: 7.735974391878129 }
-  zoom:number = 18
+  zoom:number = 13
   directionService?: google.maps.DirectionsService;
   //marker
   ngOnInit(){
-    this.dataStorage.inviaRichiesta("get", "/Perizie")?.subscribe((res)=>{
-      console.log(res);
-      this.markerList = res;
-    })
+    // this.dataStorage.inviaRichiesta("get", "/Perizie")?.subscribe((res)=>{
+    //   console.log("OK")
+    //   console.log(res);
+    //   this.markerList = res;
+    // })
+    this.markerList = [
+        {
+            "città": "Fossano",
+            "coords": {"lat": 44.5523731929605, "lng": 7.721713232773531},
+            "data": {
+                "giorno": "Lunedì",
+                "mese": "Giugno",
+                "anno": 2020,
+                "date": "01/06/2020"
+            },
+            "ora": "10:30",
+            "descrizione": "Valutazione dei danni strutturali in un edificio residenziale.",
+            "immagini": ["https://example.com/danni_strutturali_1.jpg", "https://example.com/danni_strutturali_2.jpg"],
+            "utente": "Franco",
+            "colore": "blu"
+        },
+        {
+            "città": "Fossano",
+            "coords": {"lat": 44.55079932397518, "lng": 7.72894500967683},
+            "data": {
+                "giorno": "Giovedì",
+                "mese": "Febbraio",
+                "anno": 2022,
+                "date": "13/02/2022"
+            },
+            "ora": "14:15",
+            "descrizione": "Perizia tecnica su un incidente stradale.",
+            "immagini": ["https://example.com/incidente_stradale_1.jpg", "https://example.com/incidente_stradale_2.jpg"],
+            "utente": "Giovanni",
+            "colore": "rosso"
+        },
+        {
+            "città": "Scarnafigi",
+            "coords": {"lat": 44.6817694598481, "lng": 7.569922421409666},
+            "data": {
+                "giorno": "Mercoledì",
+                "mese": "Aprile",
+                "anno": 2023,
+                "date": "21/05/2023"
+            },
+            "ora": "9:30",
+            "descrizione": "Valutazione della conformità normativa di un impianto industriale.",
+            "immagini": ["https://example.com/conformita_normativa_1.jpg", "https://example.com/conformita_normativa_2.jpg"],
+            "utente": "Piero",
+            "colore": "verde"
+        },
+        {
+            "città": "Saluzzo",
+            "coords": {"lat": 44.64707511066441, "lng": 7.499720382876884},
+            "data": {
+                "giorno": "Domenica",
+                "mese": "Ottobre",
+                "anno": 2024,
+                "date": "28/10/2024"
+            },
+            "ora": "8:15",
+            "descrizione": "Perizia di valutazione immobiliare per scopi fiscali.",
+            "immagini": ["https://example.com/valutazione_immobiliare_1.jpg", "https://example.com/valutazione_immobiliare_2.jpg"],
+            "utente": "Guglielmo",
+            "colore": "arancione"
+        },
+        {
+            "città": "Saluzzo",
+            "coords": {"lat": 44.64018257347816, "lng": 7.498800278742563},
+            "data": {
+                "giorno": "Domenica",
+                "mese": "Dicembre",
+                "anno": 2022,
+                "date": "22/09/2022"
+            },
+            "ora": "17:22",
+            "descrizione": "Perizia su danni causati da eventi atmosferici.",
+            "immagini": ["https://example.com/danni_eventi_atmosferici_1.jpg", "https://example.com/danni_eventi_atmosferici_2.jpg"],
+            "utente": "Giovanni",
+            "colore": "rosso"
+        },
+        {
+            "città": "Fossano",
+            "coords": {"lat": 44.55010714003419, "lng": 7.7225220816183},
+            "data": {
+                "giorno": "Lunedì",
+                "mese": "Novembre",
+                "anno": 2023,
+                "date": "13/02/2022"
+            },
+            "ora": "9:45",
+            "descrizione": "Perizia di valutazione danni in un'area industriale.",
+            "immagini": ["https://example.com/danni_industriali_1.jpg", "https://example.com/danni_industriali_2.jpg"],
+            "utente": "Franco",
+            "colore": "blu"
+        },
+        {
+            "città": "Savigliano",
+            "coords": {"lat": 44.64881849585665, "lng": 7.662144808499779},
+            "data": {
+                "giorno": "Venerdì",
+                "mese": "Agosto",
+                "anno": 2023,
+                "date": "15/08/2023"
+            },
+            "ora": "10:20",
+            "descrizione": "Perizia tecnica per un incidente in un cantiere edile.",
+            "immagini": ["https://example.com/incidente_cantiere_1.jpg", "https://example.com/incidente_cantiere_2.jpg"],
+            "utente": "Giovanni",
+            "colore": "rosso"
+        },
+        {
+            "città": "San Salvatore",
+            "coords": {"lat": 44.6065723616791, "lng": 7.607579661496507},
+            "data": {
+                "giorno": "Sabato",
+                "mese": "Venerdì",
+                "anno": 2024,
+                "date": "11/02/2024"
+            },
+            "ora": "11:20",
+            "descrizione": "Perizia su danni strutturali causati da terremoto.",
+            "immagini": ["https://example.com/danni_terremoto_1.jpg", "https://example.com/danni_terremoto_2.jpg"],
+            "utente": "Piero",
+            "colore": "verde"
+        }
+    ]
   }
   onMarkerClick(event: google.maps.MapMouseEvent, marker: any){
-    console.log(marker.coords)
     // this.mapService.markerCoords = JSON.stringify(marker.coords)
     this.mapService.markerCoordsObservable.next(marker);
+    this.flag = true;
+
   }
+
 
 }
--- src/app/home/info-window/info-window.component.ts
+++ src/app/home/info-window/info-window.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap, GoogleMapsModule, MapAdvancedMarker } from '@angular/google-maps';
 import { MapService } from 'src/app/shared/map.service';
 
 @Component({
@@ -7,40 +8,68 @@ import { MapService } from 'src/app/shared/map.service';
   templateUrl: './info-window.component.html',
   styleUrls: ['./info-window.component.scss'],
   standalone: true,
-  imports: [GoogleMapsModule]
+  imports: [GoogleMapsModule, NgIf, NgClass]
 })
-export class InfoWindowComponent  implements OnInit {
-
+export class InfoWindowComponent{
+  @ViewChild('mapElement') mapElement!: any;
+  map?: google.maps.Map;
   constructor(public mapService:MapService) { }
+  flag:boolean = false;
   lat?:number;
   lng?:number;
   city?:string;
   coords!:google.maps.LatLngLiteral;
-
+  mapOptions!: google.maps.MapOptions;
+  travelDuration?:string;
+  user?:string;
+  date?:string;
   directionsService!: google.maps.DirectionsService;
   directionsRenderer!: google.maps.DirectionsRenderer;
-  directionOptions: google.maps.DirectionsRequest = {
-    origin: { lat: 44.55577411467918, lng: 7.735974391878129 },
-    destination: this.coords,
-    travelMode: google.maps.TravelMode.DRIVING
-  }
+  directionOptions!: google.maps.DirectionsRequest;
+  ngAfterViewInit() {
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer({
+      polylineOptions: {
+        strokeColor: 'pink',
+        strokeOpacity: 1,
+        strokeWeight: 7
+      }
+    });
+    this.mapOptions = {
+      zoomControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      fullscreenControl: false,
+      center: this.coords,
+      zoom: 10
 
-  ngOnInit() {
-    setInterval(()=>{
-      console.log(this.mapService.markerCoords)
-    }, 1000)
+    };
+    
+    this.map = new google.maps.Map(this.mapElement.nativeElement, this.mapOptions);
+    this.directionsRenderer.setMap(this.map);
     this.mapService.markerCoordsObservable.subscribe((marker:any)=>{
+      this.flag = true;
       this.lat = marker.coords.lat;
       this.lng = marker.coords.lng;
       this.city = marker.città;
       this.coords = marker.coords;
-    })
-    this.directionsService.route(this.directionOptions, (result, status) => {
-      if (status === 'OK') {
-        // this.directionsRenderer.setDirections(result);
-        console.log("OK")
+      this.user = marker.utente;
+      this.date = marker.data.date;
+
+      this.directionOptions = {
+        origin: { lat: 44.55577411467918, lng: 7.735974391878129 },
+        destination: this.coords,
+        travelMode: google.maps.TravelMode.DRIVING
       }
-    });
+  
+      this.directionsService.route(this.directionOptions, (result, status) => {
+        if (status === 'OK' && result) {
+          this.directionsRenderer.setDirections(result);
+          this.travelDuration = result.routes[0].legs[0].duration?.text;
+        }
+      });
+    })
   }
   
   
