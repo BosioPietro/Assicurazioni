238c72e6251621d05c5dcd8a1dd8677a288caed5
lorenzosantini05 <105849734+lorenzosantini05@users.noreply.github.com>
2024-04-16 23:47:36 +0200

    Migliorata grafica select

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,15 +1,25 @@
+import { NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {IonIcon} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
+  imports: [IonIcon, NgClass],
   standalone: true
 })
 export class SelectComponent  implements OnInit {
 
   constructor() { }
-
+  elencoOperatori:any [] = ["Franco", "Giovanni", "Guglielmo", "Piero"];
+  flagOpen: boolean = false;
   ngOnInit() {}
 
+  openSelect(){
+    this.flagOpen = !this.flagOpen;
+  }
+  optionCliccata(operatore:any){
+    this.flagOpen = false;
+  }
 }
