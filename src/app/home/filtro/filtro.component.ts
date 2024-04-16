5f2e73e26c111d50601e7ed008a00d082deab43f
lorenzosantini05 <l.santini.2310@vallauri.edu>
2024-04-16 13:22:26 +0200

    Iniziato select component

--- src/app/home/filtro/filtro.component.ts
+++ src/app/home/filtro/filtro.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {IonIcon} from "@ionic/angular/standalone";
+import { SelectComponent } from 'src/app/components/select/select.component';
 
 @Component({
   selector: 'app-filtro',
   templateUrl: './filtro.component.html',
   styleUrls: ['./filtro.component.scss'],
-  imports: [IonIcon],
+  imports: [IonIcon, SelectComponent],
   standalone: true
 })
 export class FiltroComponent  implements OnInit {
