0f0c33a7274d3140baf1fcf2a42fb3fa6bfa8b31
BosioPietro <p.bosio.2155@vallauri.edu>
2024-04-29 19:19:21 +0200

    Added main page loader

--- src/app/perizia/dialogo-immagini/dialogo-immagini.component.ts
+++ src/app/perizia/dialogo-immagini/dialogo-immagini.component.ts
@@ -28,36 +28,7 @@ export class DialogoImmaginiComponent implements AfterViewInit{
 
   ngAfterViewInit() {
     this.modale.nativeElement.showModal();
-    this.MuoviImmagine(0, true)
-
-    setTimeout(() => {
-      this.contImmagini.nativeElement.classList.add("transizione")
-    });
   }
 
-  W_IMMAGINE = 30
-  GAP = 2
-
-  Immagine(i: number){
-    const immagini = this.contImmagini.nativeElement.querySelectorAll("img");
-    immagini.forEach((i) => i.classList.remove("selezionata"))
-    return Array.from(immagini)[i]
-  }
-  
-  MuoviImmagine(indice: number, togliTransizione: boolean = false){
-    const img = this.Immagine(indice);
-    const cont = this.contImmagini.nativeElement;
-
-    const wImmagine = RemInPx(img.style.getPropertyValue("--w-immagine"))
-    const gap = RemInPx(cont.style.getPropertyValue("--gap"))
-
-    let wFinale = 0;
-    for(let i = 0; i < indice; ++i){
-      wFinale += wImmagine + gap;
-    }
-
-    cont.style.setProperty("--x", `${-wFinale}px`)
-    img.classList.add("selezionata")
-    console.log(img)
-  }
+  indice = 0;
 }
